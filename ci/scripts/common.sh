c9e34d33d2b2ea7bd0c55df463505888c9224a1e
Concourse <nobody@concourse.ci>
2016-08-25 10:12:48 +0100

    draft concourse stuff

---
+++ ci/scripts/common.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+set -e
+APPNAME=cities-ui
+
+abort()
+{
+    echo >&2 '
+    ***************
+    *** ABORTED ***
+    ***************
+    '
+    echo "An error occurred. Exiting..." >&2
+    exit 1
+}
+
+echo_msg()
+{
+  echo ""
+  echo "************** ${1} **************"
+}
+
+cf_login()
+{
+  cf --version
+  cf login -a $api -u $username -p $password -o $organization -s $space $ssl
+}
+
+exitIfNull()
+{
+  if [ -z "${1}" ]
+  then
+    exit 1
+  fi
+}
--- ci/scripts/testAppOnPCF.sh
+++ ci/scripts/testAppOnPCF.sh
@@ -1,7 +1,33 @@
-#!/bin/bash 
+#!/bin/sh 
+. $APPNAME/ci/scripts/common.sh
 
-running=`curl -s $1 | grep $2`
-if [-z $running]
-then
- exit -1 
-fi
+checkAppIsDeployed()
+{
+  URL=`cf apps | grep $APPNAME | xargs | cut -d " " -f 6`
+  exitIfNull $URL
+
+  running=`curl -s $URL/health | grep '"status" : "UP"' `
+  exitIfNull $running
+}
+
+searchForCity()
+{
+  running=`curl -s $URL | grep "Aldermoor"`
+  exitIfNull $running
+}
+
+main()
+{
+  cf_login
+  checkAppIsDeployed
+  searchForCity
+  cf logout
+}
+
+trap 'abort $LINENO' 0
+SECONDS=0
+SCRIPTNAME=`basename "$0"`
+
+main
+printf "\nExecuted $SCRIPTNAME in $SECONDS seconds.\n"
+trap : 0
