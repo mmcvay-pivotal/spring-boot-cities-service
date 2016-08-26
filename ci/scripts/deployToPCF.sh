d24392174d3ca0c5a226c017cfdf11544d20a35b
Concourse <nobody@concourse.ci>
2016-08-26 09:57:52 +0100

    Playingi with versions and routes

--- ci/scripts/deployToPCF.sh
+++ ci/scripts/deployToPCF.sh
@@ -19,7 +19,7 @@ push()
 
   echo "Pushing Live Route"
   DOMAIN=`cf domains | grep shared | head -n 1 | cut -d" " -f1`
-  cf map-route $APP_NAME $CF_DOMAIN -n $APPNAME-$CF_USER
+  cf map-route $APP_NAME $CF_DOMAIN -n $APPNAME-$username
 }
 
 main()
