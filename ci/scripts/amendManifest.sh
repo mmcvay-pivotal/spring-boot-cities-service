bdd6aa0ddfa7c4eaa26b9fcfc0d3bc030ee82a45
Concourse <nobody@concourse.ci>
2016-08-26 18:40:32 +0100

    Playing with versions and routes

--- ci/scripts/amendManifest.sh
+++ ci/scripts/amendManifest.sh
@@ -1,26 +1,16 @@
-#!/bin/sh 
+#!/bin/sh
 . $APPNAME/ci/scripts/common.sh
-
-addVersionEnv()
-{
-  cf set-env $APPNAME VERSION $VERSION
-}
 
 main()
 {
-  cf_login
-
-  summaryOfApps
-  createVarsBasedOnVersion
-  addVersionEnv
-  summaryOfApps
-
-  cf logout
+  cd $APPNAME
+  more manifest.yml
 }
 
 trap 'abort $LINENO' 0
 SECONDS=0
 SCRIPTNAME=`basename "$0"`
+export TERM=${TERM:-dumb}
 main
 printf "\nExecuted $SCRIPTNAME in $SECONDS seconds.\n"
 trap : 0
