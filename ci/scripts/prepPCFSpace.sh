df057baab6a8bba8ed124c6f9a84234c851ab9f3
Concourse <nobody@concourse.ci>
2016-08-29 22:08:56 +0100

    Initial concourse jobs

--- ci/scripts/prepPCFSpace.sh
+++ ci/scripts/prepPCFSpace.sh
@@ -2,6 +2,17 @@
 set -e
 . $APPNAME/ci/scripts/common.sh
 
+create_service()
+{
+  service_created=0
+  EXISTS=`cf services | grep ${1} | wc -l | xargs`
+  if [ $EXISTS -eq 0 ]
+  then
+    cf create-service ${1} ${2} ${3}
+    service_created=1
+  fi
+}
+
 main()
 {
   cf_login 
@@ -15,6 +26,17 @@ main()
       PLAN=`cf marketplace -s p-mysql | grep MySQL | head -n 1 | cut -d ' ' -f1 | xargs`
     fi
     cf create-service p-mysql $PLAN ${SERVICE_NAME}
+  fi
+
+  create_service p-service-registry standard $DISCOVERY
+  if [ $service_created -eq 1 ]
+  then
+    # Sleep for service registry
+    max=12
+    for ((i=1; i<=$max; ++i )) ; do
+      echo "Pausing to allow Service Discovery to Initialise.....$i/$max"
+      sleep 5
+    done
   fi
   summaryOfServices
   cf logout
